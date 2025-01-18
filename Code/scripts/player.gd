0f9150329de1b1faeaecd2bd80d62f6a80c73511
SamJoffy <sajoffy@gmail.com>
2025-01-18 14:58:49 -0500

    Fixed multiplayer rotation and visibility issues

--- Code/scripts/player.gd
+++ Code/scripts/player.gd
@@ -11,6 +11,7 @@ extends CharacterBody3D
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
 @export var CAMERA_CONTROLLER : Camera3D
 @export var RAYCAST : RayCast3D
+@export var PLAYERMODEL : Node3D
 
 @onready var LEFTRAYCAST: RayCast3D = %LeftRayCast
 @onready var RIGHTRAYCAST: RayCast3D = %RightRayCast
@@ -26,6 +27,7 @@ var _camera_rotation : Vector3
 var _current_rotation : float
 var _tilt_input: float = 0.0
 var jump_ready: bool = true
+var isFocused: bool = true
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
@@ -33,42 +35,55 @@ func _enter_tree():
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	CAMERA_CONTROLLER.current = is_multiplayer_authority()
+	if is_multiplayer_authority():
+		for _i in PLAYERMODEL.get_children():
+			_i.set_layer_mask_value(3, false)
 
 func _physics_process(delta):
 	_update_camera(delta)
 
+func _notification(what):
+	match what:
+		NOTIFICATION_WM_WINDOW_FOCUS_OUT:
+			isFocused = false
+		NOTIFICATION_WM_WINDOW_FOCUS_IN:
+			isFocused = true
+
 func _input(event):
-	if event.is_action_pressed("exit"):
-		$"../".exit_game(name.to_int())
-		get_tree().quit()
-	elif event.is_action_pressed("shoot"):
-		if RAYCAST.is_colliding():
-			print("hit")
-		else:
-			print("miss")
+	if is_multiplayer_authority():
+		if event.is_action_pressed("exit"):
+			$"../".exit_game(name.to_int())
+			get_tree().quit()
+		elif event.is_action_pressed("shoot"):
+			if RAYCAST.is_colliding():
+				print("hit")
+			else:
+				print("miss")
 
 func _unhandled_input(event):
-	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	if _mouse_input:
-		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
-		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+	if is_multiplayer_authority():
+		_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+		if _mouse_input:
+			_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+			_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
 
 func _update_camera(delta):
-	_current_rotation = _rotation_input
-	_mouse_rotation.x += _tilt_input * delta
-	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
-	_mouse_rotation.y += _rotation_input * delta
-	
-	_player_rotation = Vector3(0.0, _mouse_rotation.y, 0.0)
-	_camera_rotation = Vector3(_mouse_rotation.x, 0.0, 0.0)
-	
-	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
-	CAMERA_CONTROLLER.rotation.z = 0.0
-	
-	global_transform.basis = Basis.from_euler(_player_rotation)
-	
-	_rotation_input = 0.0
-	_tilt_input = 0.0
+	if isFocused and is_multiplayer_authority():
+		_current_rotation = _rotation_input
+		_mouse_rotation.x += _tilt_input * delta
+		_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+		_mouse_rotation.y += _rotation_input * delta
+		
+		_player_rotation = Vector3(0.0, _mouse_rotation.y, 0.0)
+		_camera_rotation = Vector3(_mouse_rotation.x, 0.0, 0.0)
+		
+		CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+		CAMERA_CONTROLLER.rotation.z = 0.0
+		
+		global_transform.basis = Basis.from_euler(_player_rotation)
+		
+		_rotation_input = 0.0
+		_tilt_input = 0.0
 
 func update_gravity(delta, isWallRunning: bool = false) -> void:
 	if not isWallRunning:
@@ -77,32 +92,33 @@ func update_gravity(delta, isWallRunning: bool = false) -> void:
 		velocity.y = 0.0
 
 func update_input(speed: float, acceleration: float, deceleration: float, isWallRunning: bool = false) -> void:
-	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = lerp(velocity.x, direction.x * speed, acceleration)
-		velocity.z = lerp(velocity.z, direction.z * speed, acceleration)
-	else:
-		velocity.x = move_toward(velocity.x, 0, deceleration)
-		velocity.z = move_toward(velocity.z, 0, deceleration)
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jump_ready and not isWallRunning:
-		jump_ready = false
-		velocity.y = JUMP_VELOCITY
-		await get_tree().create_timer(0.2).timeout
-		jump_ready = true
-	elif Input.is_action_just_pressed("jump") and isWallRunning:
-		var forwardDirection = Vector3(1, 0, 1)
-		if LEFTRAYCAST.is_colliding():
-			velocity = LEFTRAYCAST.get_collision_normal() * JUMP_VELOCITY * 10
-			forwardDirection -= LEFTRAYCAST.get_collision_normal()
-		elif RIGHTRAYCAST.is_colliding():
-			velocity = RIGHTRAYCAST.get_collision_normal() * JUMP_VELOCITY * 10
-			forwardDirection -= RIGHTRAYCAST.get_collision_normal()
-		var forwardVelocity = forwardDirection * speed
-		velocity += forwardVelocity
-	
+	if is_multiplayer_authority():
+		var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = lerp(velocity.x, direction.x * speed, acceleration)
+			velocity.z = lerp(velocity.z, direction.z * speed, acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, deceleration)
+			velocity.z = move_toward(velocity.z, 0, deceleration)
+		
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and jump_ready and not isWallRunning:
+			jump_ready = false
+			velocity.y = JUMP_VELOCITY
+			await get_tree().create_timer(0.2).timeout
+			jump_ready = true
+		elif Input.is_action_just_pressed("jump") and isWallRunning:
+			var forwardDirection = Vector3(1, 0, 1)
+			if LEFTRAYCAST.is_colliding():
+				velocity = LEFTRAYCAST.get_collision_normal() * JUMP_VELOCITY * 10
+				forwardDirection -= LEFTRAYCAST.get_collision_normal()
+			elif RIGHTRAYCAST.is_colliding():
+				velocity = RIGHTRAYCAST.get_collision_normal() * JUMP_VELOCITY * 10
+				forwardDirection -= RIGHTRAYCAST.get_collision_normal()
+			var forwardVelocity = forwardDirection * speed
+			velocity += forwardVelocity
 
 func update_velocity() -> void:
-	move_and_slide()
+	if is_multiplayer_authority():
+		move_and_slide()
